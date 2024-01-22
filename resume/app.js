6fd5939359c2a1d36bb7046021c9313771f4dd26
mohamadsyalvasr <mohamadsyalva@outlook.com>
2024-01-23 03:58:15 +0700

    update

--- resume/app.js
+++ resume/app.js
@@ -49,11 +49,13 @@ fetch("data/projects.json")
 function renderHeader(data) {
   return `
     <h1>${data.name}</h1>
-    <p><a href="mailto:${data.email}">${data.email}</a> | <a href="wa.me/${
+    <p><a href="mailto:${data.email}">${
+    data.email
+  }</a> | <a href="https://wa.me/${data.phone}" target="_blank">${
     data.phone
-  }" target="_blank">${data.phone}</a> | <a href="${
-    data.linkedIn
-  }" target="_blank">${data.linkedIn}</a> | ${data.address}</p>
+  }</a> | <a href="${data.linkedIn}" target="_blank">${data.linkedIn}</a> | ${
+    data.address
+  }</p>
     <div class="about-me">
       ${data.aboutMe.map((paragraph) => `<p>${paragraph}</p>`).join("")}
     </div>
@@ -71,15 +73,17 @@ function renderExperience(data) {
     );
 
     const showReadMoreButton = experience.description.length > 10;
+    const starDate = handlerLocalString(experience.startDate);
+    const endDate = handlerLocalString(experience.endDate);
 
     experienceHTML += `
       <div class="card mb-3">
         <div class="card-body">
           <h5 class="card-title">${experience.position}</h5>
           <p class="card-text">
-            <strong>${experience.company}</strong> | ${
-      experience.startDate
-    } - ${experience.endDate} (${duration})
+            <strong>${
+              experience.company
+            }</strong> <br> ${starDate} - ${endDate} (${duration})
           </p>
           <div class="description-container">
             <p class="card-text description" style="display: none;">${
@@ -87,7 +91,7 @@ function renderExperience(data) {
             }</p>
             ${
               showReadMoreButton
-                ? '<button class="btn btn-link read-more-btn">Show Details</button>'
+                ? '<button class="btn btn-link read-more-btn w-100">Show Details</button>'
                 : ""
             }
           </div>
@@ -102,6 +106,8 @@ function renderExperience(data) {
 // Function to render education section
 function renderEducation(data) {
   let educationHTML = "<h2>Education</h2>";
+  let showReadMoreButton = true;
+
   data.forEach((item) => {
     const detailsHTML = renderDetails(item.description);
 
@@ -121,9 +127,20 @@ function renderEducation(data) {
       educationHTML += `
         <div class="card mb-3">
           <div class="card-body">
-            <h5 class="card-title">${item.course} (Course)</h5>
-            <p class="card-text">${item.institution} | ${item.location} | ${item.date}</p>
+            <h5 class="card-title"><i>(Course)</i> ${item.course}</h5>
+          <p class="card-text">
+            <span class="institution">${item.institution}</span> |
+            <span class="location">${item.location}</span> |
+            <span class="date">${item.date}</span>
+          </p>
+            <div class="description-container">
             ${detailsHTML}
+            ${
+              showReadMoreButton
+                ? '<button class="btn btn-link read-more-btn w-100">Show Details</button>'
+                : ""
+            }
+          </div>
           </div>
         </div>
       `;
@@ -137,13 +154,13 @@ function renderDetails(details) {
 
   // Check if details is an array
   if (Array.isArray(details)) {
-    detailsHTML += "<ul>";
+    detailsHTML += `<ul class="card-text description" style="display: none;">`;
     details.forEach((detail) => {
       detailsHTML += `<li>${detail}</li>`;
     });
     detailsHTML += "</ul>";
   } else {
-    detailsHTML += `<p>${details}</p>`;
+    detailsHTML += `<p class="card-text description" style="display: none;">${details}</p>`;
   }
 
   return detailsHTML;
@@ -202,17 +219,45 @@ function renderSkillsAchievements(data) {
 function renderCertifications(data) {
   let certificationsHTML = "<h2>Licenses & Certifications</h2>";
 
+  data.sort((a, b) => {
+    if (!a.expirationYear && !b.expirationYear) return 0; // Jika keduanya tidak memiliki tanggal kedaluwarsa
+    if (!a.expirationYear) return -1; // Jika hanya sertifikat A yang tidak memiliki tanggal kedaluwarsa
+    if (!b.expirationYear) return 1; // Jika hanya sertifikat B yang tidak memiliki tanggal kedaluwarsa
+
+    const dateA = new Date(a.expirationYear);
+    const dateB = new Date(b.expirationYear);
+
+    // Urutkan dari yang terbesar (paling lama terlebih dahulu)
+    const comparison = dateB - dateA;
+
+    // Balik urutan jika sertifikat telah kedaluwarsa
+    return a.expirationYear ? comparison : -comparison;
+  });
+
   data.forEach((certification) => {
+    let expirationYear;
+    const expirationInfo = calculateExpirationInfo(
+      certification.expirationYear
+    );
+
+    const issueYear = handlerLocalString(certification.issueYear);
+
+    if (certification.expirationYear) {
+      expirationYear = handlerLocalString(certification.expirationYear);
+    } else {
+      expirationYear = expirationInfo;
+    }
+
     certificationsHTML += `
       <div class="card mb-3">
         <div class="card-body">
           <h5 class="card-title">${certification.name}</h5>
-          <p class="card-text">${certification.issuedBy} | ${
-      certification.issueYear
-    } | ${
+          <p class="card-text">Issued by <b><i>${
+            certification.issuedBy
+          }</i></b> on ${issueYear} <br>${
       certification.expirationYear
-        ? `Expires ${certification.expirationYear}`
-        : "No Expired Date"
+        ? `Expired on ${expirationYear} (${expirationInfo})`
+        : `<i>This certification does not expire</i>`
     }`;
 
     // Check if Credential ID exists before adding it to the paragraph
@@ -222,7 +267,23 @@ function renderCertifications(data) {
 
     // Check if Verification Link exists before adding it to the paragraph
     if (certification.verificationLink) {
-      certificationsHTML += `<br>Verify: <a href="${certification.verificationLink}" target="_blank">${certification.verificationLink}</a>`;
+      certificationsHTML += `<br><a href="${certification.verificationLink}" target="_blank">Verify Credential</a>`;
+    }
+
+    if (certification.description) {
+      let showReadMoreButton = certification.description.length > 10;
+
+      certificationsHTML += `<div class="description-container">
+      <hr>
+            <p class="card-text description" style="display: none;">${
+              certification.description
+            } </p>
+            ${
+              showReadMoreButton
+                ? '<button class="btn btn-link read-more-btn w-100">Show Details</button>'
+                : ""
+            }
+          </div>`;
     }
 
     certificationsHTML += `</p>
@@ -244,16 +305,17 @@ function renderProjects(data) {
     );
 
     let detailsHTML = "";
+    let showReadMoreButton = true;
 
     // Check if details is an array
     if (Array.isArray(project.details)) {
-      detailsHTML += "<ul>";
+      detailsHTML += `<ul class="card-text description" style="display: none;">`;
       project.details.forEach((detail) => {
         detailsHTML += `<li>${detail}</li>`;
       });
       detailsHTML += "</ul>";
     } else {
-      detailsHTML += `<p>${project.details}</p>`;
+      detailsHTML += `<p class="card-text description" style="display: none;">${project.details}</p>`;
     }
 
     projectsHTML += `
@@ -267,7 +329,14 @@ function renderProjects(data) {
           }
           <br><i>${project.role}</i>
           <br>${project.startDate} - ${project.endDate} (${duration})</p>
-          ${detailsHTML}
+          <div class="description-container">
+            ${detailsHTML}
+            ${
+              showReadMoreButton
+                ? '<button class="btn btn-link read-more-btn w-100">Show Details</button>'
+                : ""
+            }
+          </div>
         </div>
       </div>
     `;
@@ -289,14 +358,36 @@ function calculateExperienceDuration(startDate, endDate) {
     return "Present";
   }
 
-  const years = end.getFullYear() - start.getFullYear();
-  const months = end.getMonth() - start.getMonth();
+  let years = end.getFullYear() - start.getFullYear();
+  let months = end.getMonth() - start.getMonth();
+  let days = end.getDate() - start.getDate();
+
+  if (days < 0) {
+    const lastMonth = new Date(end.getFullYear(), end.getMonth(), 0);
+    days += lastMonth.getDate();
+    months--;
+  }
 
   if (months < 0) {
-    return `${years - 1} years ${12 + months} months`;
-  } else {
-    return `${years} years ${months} months`;
+    months += 12;
+    years--;
   }
+
+  let duration = "";
+
+  if (years > 0) {
+    duration += `${years} ${years > 1 ? "years" : "year"} `;
+  }
+
+  if (months > 0) {
+    duration += `${months} ${months > 1 ? "months" : "month"} `;
+  }
+
+  if (days > 0) {
+    duration += `${days} ${days > 1 ? "days" : "day"}`;
+  }
+
+  return duration.trim();
 }
 
 document.addEventListener("click", function (event) {
@@ -364,9 +455,68 @@ function updateDetik() {
   // Tampilkan hasil
   document.getElementById(
     "result"
-  ).innerHTML = `<i>Since my birth, more than ${differenceInSeconds} seconds have passed, allowing me to adapt easily to different tasks and responsibilities.</i>`;
+  ).innerHTML = `<i>Since my birth, more than ${differenceInSeconds} seconds have passed, allowing me to easily adapt to different tasks and responsibilities.</i>`;
 }
 
 // Update detik setiap detiknya
 updateDetik(); // Update pertama kali
 setInterval(updateDetik, 1000); // Update setiap 1 detik
+
+function calculateExpirationInfo(expirationDate) {
+  if (!expirationDate) {
+    return "No expiration date";
+  }
+
+  const now = new Date();
+  const expiration = new Date(expirationDate);
+
+  const daysRemaining = Math.floor((expiration - now) / (1000 * 60 * 60 * 24));
+
+  if (daysRemaining > 0) {
+    return `<i>Expires in ${daysRemaining} days</i>`;
+  } else if (daysRemaining === 0) {
+    return "<i>Expires today</i>";
+  } else {
+    return "<i>Expired</i>";
+  }
+}
+
+// Fungsi untuk menangani peningkatan dan penurunan ukuran teks
+function changeFontSize(change) {
+  const currentFontSize = parseInt(document.body.style.fontSize) || 16; // Ukuran font awal (16px jika tidak ada)
+  const newFontSize = Math.max(10, currentFontSize + change); // Hindari ukuran font kurang dari 10px
+
+  document.body.style.fontSize = `${newFontSize}px`;
+}
+
+// Fungsi event listener untuk tombol + dan -
+function handleFontSizeButtonClick(event) {
+  const buttonId = event.target.id;
+
+  if (buttonId === "increase-font-size") {
+    changeFontSize(2); // Peningkatan ukuran font sebesar 2px
+  } else if (buttonId === "decrease-font-size") {
+    changeFontSize(-2); // Penurunan ukuran font sebesar 2px
+  }
+}
+
+function handlerLocalString(date) {
+  if (date === "Present" || date === "") {
+    return "Present";
+  }
+
+  const newFormat = new Date(date).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+  });
+
+  return newFormat;
+}
+
+// Tambahkan event listener untuk tombol + dan -
+const increaseFontSizeButton = document.getElementById("increase-font-size");
+const decreaseFontSizeButton = document.getElementById("decrease-font-size");
+
+increaseFontSizeButton.addEventListener("click", handleFontSizeButtonClick);
+decreaseFontSizeButton.addEventListener("click", handleFontSizeButtonClick);
